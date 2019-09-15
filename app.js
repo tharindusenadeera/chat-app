af53603228923b55742c34fc622c91a49e34bac9
Tharindu Senadeera <tharindu@rydecars.com>
2019-09-15 23:49:55 +0530

    configured and initialized express and socket.io

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const http = require('http')
+
+const app = express()
+
+//set the template engine ejs
+app.set('view engine', 'ejs')
+
+//middlewares
+app.use(express.static('public'))
+
+//routes
+app.route('/', (req,res)  =>{
+    res.send('Hello World!!!')
+})
+
+//listing on server
+const server = http.createServer(app).listen(8080, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      const host = server.address().address;
+      const port = server.address().port;
+      console.log(`Server listening on ${host}:${port}`);
+    }
+  })
+
+//socket.io installation
+const io = require("socket.io")(server)
+
+//listning on connection
+io.on('connection', (socket) => {
+    console.log("New user is connected")
+    
+})
